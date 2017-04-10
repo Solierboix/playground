29c26f3676a6fc88367cd1293a427d75779fb2b7
walczpio <piotr.walczak@tieto.com>
2017-04-10 16:31:40 +0200

    Add ToolBar with included overflow menu.
    Added ToolBar that includes "About" item in overflow menu.
    Additionally title on the bar was changed.
    
    Change-Id: Id0d0abc5d8cbb5274a8fb5349d3e8dc3e5900d16

--- app/src/main/java/com/example/walczpio/task1/MainActivity.java
+++ app/src/main/java/com/example/walczpio/task1/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.walczpio.task1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Toolbar toolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
