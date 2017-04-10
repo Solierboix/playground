093fc81897befe4f0cdbbf1c037e0e3729be394f
Solierboix <solierboix1991@gmail.com>
2017-04-11 00:24:40 +0200

    updated
    Change-Id: I7a3f32fedfc2b82cb5fa01a489d5f4e2234754b1

--- app/src/main/java/com/example/walczpio/task1/MainActivity.java
+++ app/src/main/java/com/example/walczpio/task1/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.walczpio.task1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView BrithnessActivityStart;
     Toolbar toolBar;
+    Intent enterBrithnessActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         toolBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolBar);
 
+        BrithnessSettingsTextView();
+
+    }
+
+    private void BrithnessSettingsTextView() {
+        BrithnessActivityStart = (TextView) findViewById(R.id.brithnessAactivityEnter);
+        BrithnessActivityStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterBrithnessActivity = new Intent(MainActivity.this, BrithnessSettingsActivity.class);
+                MainActivity.this.startActivity(enterBrithnessActivity);
+            }
+        });
     }
 
     @Override
